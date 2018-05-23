efce43cb2f4758fd981a48c7d1f47d8036417e22
Armando L <shellfirexd@gmail.com>
2018-05-22 22:04:29 -0600

    limpieza de codigo

---
+++ Ethernet_4.ino.ino
@@ -0,0 +1,133 @@
+// DHT Temperature & Humidity Sensor
+// Unified Sensor Library Example
+// Written by Tony DiCola for Adafruit Industries
+// Released under an MIT license.
+
+// Depends on the following Arduino libraries:
+// - Adafruit Unified Sensor Library: https://github.com/adafruit/Adafruit_Sensor
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <Ethernet.h>
+#include <SPI.h>
+
+// Configuracion del Ethernet Shield
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFF, 0xEE}; // Direccion MAC
+byte ip[] = { 192,168,0,110 }; // Direccion IP del Arduino
+IPAddress myDns(8,8,8,8);
+//byte server[] = { 192,168,0,103 }; // Direccion IP del servidor
+//char server[]="weatherstation.nicadevelop.com";
+EthernetClient client; 
+
+#define DHTPIN            2         // Pin which is connected to the DHT sensor.
+
+// Uncomment the type of sensor in use:
+//#define DHTTYPE           DHT11     // DHT 11 
+#define DHTTYPE           DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE           DHT21     // DHT 21 (AM2301)
+
+// See guide for details on sensor wiring and usage:
+//   https://learn.adafruit.com/dht/overview
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+
+void setup() {
+  Serial.begin(9600); 
+  // Initialize device.
+  dht.begin();
+   Ethernet.begin(mac, ip,myDns ); // Inicializamos el Ethernet Shield
+  Serial.println("DHTxx Unified Sensor Example");
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Temperature");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");  
+  Serial.println("------------------------------------");
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Humidity");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println("%");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println("%");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");  
+  Serial.println("------------------------------------");
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+}
+
+void loop() {
+
+  float temperatura,humedad;
+  
+  // Delay between measurements.
+  delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;  
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println("Error reading temperature!");
+  }
+  else {
+    Serial.print("Temperature: ");
+    temperatura=event.temperature;
+    Serial.print(temperatura);
+    Serial.println(" *C");
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println("Error reading humidity!");
+  }
+  else {
+    Serial.print("Humidity: ");
+    humedad=event.relative_humidity;
+    Serial.print(humedad);
+    Serial.println("%");
+  }
+
+  if(temperatura!=0 and humedad!=0){
+int segundos=0;
+
+if (segundos = 60){
+
+    // Proceso de envio de muestras al servidor
+  Serial.println("Connecting...");
+  if (client.connect("weatherstation.nicadevelop.com", 80)>0) {  // Conexion con el servidor
+    client.print("GET /arduino/controller/index.php?temp="); // Enviamos los datos por GET
+    client.print(temperatura);
+    client.print("&hum=");
+    client.print(humedad);
+    client.println(" HTTP/1.0");
+    client.println("User-Agent: Arduino 1.0");
+    client.println();
+    Serial.println("Conectado");
+  } else {
+    Serial.println("Fallo en la conexion");
+  }
+  if (!client.connected()) {
+    Serial.println("Disconnected!");
+  }
+ client.stop();
+  client.flush();
+  delay(1000); // Espero un minuto antes de tomar otra muestra
+    
+    }
+    else{
+            Serial.println("Error al enviar los datos");
+      }
+  
+  }
+ }
